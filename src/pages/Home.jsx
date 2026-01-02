4881a21c67952fde37ebc3c5aebe2c632e6046d4
sohye <104679222+brightSso@users.noreply.github.com>
2026-01-02 15:43:30 +0900

    fix: iOS 배경 영상 클릭 이슈 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,13 @@ import { tw } from "../globals";
 
 export default function Home() {
   const css = {
-    main: tw`relative flex h-screen w-full flex-wrap items-center justify-center overflow-hidden bg-light`,
-    vid: tw`absolute top-0 left-0 size-full scale-110 object-cover opacity-60 saturate-50 dark:opacity-50`
+    main: tw`relative z-0 flex h-screen w-full flex-wrap items-center justify-center overflow-hidden bg-light`,
+    vid: tw`pointer-events-none absolute top-0 left-0 -z-10 size-full scale-110 object-cover opacity-60 saturate-50 dark:opacity-50`
   };
 
   return (
     <main className={css.main}>
-      <video className={css.vid} src="./vidBg.mp4" loop muted autoPlay />
+      <video className={css.vid} src="./vidBg.mp4" loop muted autoPlay playsInline webkitPlaysInline preload="auto" />
       <Slogan />
       <QuickNav />
     </main>
